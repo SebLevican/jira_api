83dcf7f3c6af527705edc91182a038387c9b5ee8
Rolando Sebastian Serrano <59149959+SebLevican@users.noreply.github.com>
2024-11-04 14:49:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from formid import JiraHandler  # Importa la clase desde tu módulo
 from dotenv import load_dotenv
 import os
 from excel_handler import update_excel
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -14,19 +15,22 @@ jira_api_token = os.getenv('JIRA_API_TOKEN')
 
 app = FastAPI()
 
+class IssuePayload(BaseModel):
+    issue: dict
+    issue_key: str
 
 # Ruta para recibir el webhook de Jira
 @app.post('/webhook')
-async def handle_webhook(request: Request):
+async def handle_webhook(payload: IssuePayload):
+    issue_key = payload.issue.get('key')
+
     try:
-        payload = await request.json()
+        bot = JiraHandler(issue_key, jira_user, jira_api_token)
+        ask_question = bot.extract_responses()
+
+        message = update_excel(ask_question)
     except Exception as e:
-        return {'error':'invalid or missing json payload'}
-
-    bot = JiraHandler(issue_key, jira_user, jira_api_token)
-    ask_question = bot.extract_responses()
-
-    message = update_excel(ask_question)
+        return {'error':'invalid or missing json payload','details': str(e)}
 
     return {"message": message}
 
